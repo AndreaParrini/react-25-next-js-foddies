34f6d704bf4a9e21314bcbe236c09cf3331409b2
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-03-26 11:17:01 +0100

    add component image

--- components/main-header.js
+++ components/main-header.js
@@ -1,14 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import logoImg from '@/assets/logo.png';
-
 import classes from './main-header.module.css';
+
 
 export default function MainHeader(){
     return (
         <header className={classes.header}>
             <Link className={classes.logo} href='/'>
-                <img src={logoImg.src} alt='A plate with food on it'/>
+                <Image src={logoImg} alt='A plate with food on it' priority/> 
+                {/* NEL SRC ABBIAMO PASSATO L'INTERNO OGGETTO CHE SI GENERA IMPORTANDO IL LOGO IN QUANTO CI SONO INFORMAZIONI UTILI AL COMPONENTE IMAGE */}
+                {/* ABBIAMO AGGIUNTO LA PROPRITA' PRIORITY PER DIRGLI DI CARICARE L'IMMAGINA IL PRIMA POSSIBILE */}
                 Next Level Food
             </Link>
 
